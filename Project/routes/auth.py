76ce7ca803e44ad1fc23fad004d423b1b4d4463d
Elie Esses <elieesses04@gmail.com>
2026-06-07 16:15:54 -0400

    small fix

--- Project/routes/auth.py
+++ Project/routes/auth.py
@@ -66,10 +66,17 @@ def login(login_data: LoginRequest, response: Response) -> dict[str, bool]:
 
     return {"success": True}
 
+
 @router.post("/auth/logout")
-def logout(response: Response) -> dict[str, bool]:  
-    response.delete_cookie(key="access_token")
+def logout(response: Response) -> dict[str, bool]:
+    response.delete_cookie(
+        key="access_token",
+        httponly=True,
+        secure=not DEBUG_MODE,
+        samesite="lax",
+    )
     return {"success": True}
+
 
 @router.get("/auth/me")
 def me(user: UserResponse = Depends(auth.get_current_user)) -> UserResponse:
